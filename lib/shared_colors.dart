d41402dd0f8ce9e026a9cf861d993f1cd386a990
basilmarwan <basilmarwan33@hotmail.com>
2022-12-14 15:36:16 +0200

    Modified Folders

--- lib/shared_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-class AppColors {
-
-  static Color primaryColor = const Color(0x00000000);
-  static Color secondaryColor = const Color.fromARGB(255,239,239,239);
-  static Color thirdColor = const Color(0xffffffff);
-}
