71a5c6e8135704653eab6518aab4c77051da45ef
basselamr <basselkamel20@gmail.com>
2022-12-14 18:30:21 +0200

    ana bassel amr

--- lib/AppViews/inboxPage.dart
+++ lib/AppViews/inboxPage.dart
@@ -1,5 +1,3 @@
-
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, unnecessary_string_interpolations
 
 import 'package:flutter/material.dart';
@@ -26,50 +24,6 @@ class _inboxPageState extends State<inboxPage> {
 
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 239, 239, 239),
-      // appBar: AppBar(
-      //   systemOverlayStyle: SystemUiOverlayStyle(
-      //     statusBarIconBrightness: Brightness.dark,
-      //     statusBarColor: Colors.grey[350]
-      //   ),
-      //   backgroundColor: Colors.grey[350],
-      //   elevation: 0,
-      //   toolbarHeight: 300,
-      //   actions: [
-      //     Padding(
-      //       padding: const EdgeInsets.only(bottom: 200),
-      //       child: CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30,)),
-      //     SizedBox(width: 240),
-      //     Padding(
-      //       padding: const EdgeInsets.only(bottom: 200),
-      //       child: IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,)),
-      //     ),
-      //     Padding(
-      //       padding: const EdgeInsets.only(bottom: 200),
-      //       child: Badge(position: BadgePosition.topEnd(top: 35, end: 10),badgeContent: null,child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,))),
-      //     ),
-
-      //   ],
-      // ),
-      // Column(
-      //   crossAxisAlignment: CrossAxisAlignment.start,
-      //   mainAxisAlignment: MainAxisAlignment.start,
-      //   children: [
-      //     SizedBox(height: height/18),
-      //     Row(
-      //       mainAxisAlignment: MainAxisAlignment.start,
-      //       children: [
-      //         SizedBox(width: width/18),
-      //         CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30, backgroundColor: Color.fromARGB(255,239,239,239), child: IconButton(icon: Icon(color: Colors.transparent, Icons.person), onPressed: (){},)),
-      //         SizedBox(width: width/2,),
-      //         IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,), splashRadius: 25),  
-      //         Badge(position: BadgePosition.topEnd(top: 10, end: 10),badgeContent: Icon(Icons.circle ,color: Color.fromARGB(255,212,64,64), size: 2,),child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,), splashRadius: 25)),
-      //       ],
-      //     ),
-      //     SizedBox(height: height/10),
-      //     DropdownButton(items: null, onChanged: null, hint: Text("Inbox", style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold)),),
-          
-      //   ],  
-      // ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -81,23 +35,46 @@ class _inboxPageState extends State<inboxPage> {
               children: [
                 SizedBox(width: width / 18),
                 // ignore: prefer_const_constructors
-                CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30, backgroundColor: Color.fromARGB(255,239,239,239), child: IconButton(icon: Icon(color: Colors.transparent, Icons.person), onPressed: (){},)),
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),
+                  radius: 32,
+                ),
                 SizedBox(
                   width: width / 2,
                 ),
-               IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,), splashRadius: 25),
-                Badge(position: BadgePosition.topEnd(top: 10, end: 10),badgeContent: Icon(Icons.circle ,color: Color.fromARGB(255,212,64,64), size: 2,),child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,), splashRadius: 25)),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                      size: 35,
+                    )),
+                Badge(
+                    position: BadgePosition.topEnd(top: 10, end: 5),
+                    badgeContent: Icon(
+                      Icons.circle,
+                      color: Colors.red,
+                      size: 4,
+                    ),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.calendar_month,
+                          color: Colors.black,
+                          size: 35,
+                        ))),
               ],
             ),
             SizedBox(
-              height: height / 64,
+              height: height / 16,
             ),
             Container(
-              padding: EdgeInsets.only(left: width / 24,),
+              padding: EdgeInsets.only(left: width / 12),
               child: DropdownButton(
-                icon: Icon(Icons.arrow_drop_down,
-                    color: Colors.black, size: 30,),
-                items: const ["Inbox", "Drafts", "Trash", "Sent"]
+                icon: Icon(Icons.keyboard_double_arrow_down_sharp,
+                    color: Colors.black),
+                items: const ["All Inboxes", "Drafts", "Trash", "Sent"]
                     .map((e) => DropdownMenuItem(
                           child: Text("$e"),
                           value: e,
@@ -110,42 +87,18 @@ class _inboxPageState extends State<inboxPage> {
                 },
                 onTap: null,
                 hint: Text(
-                  'Inbox',
+                  'All Inboxes',
                   style: TextStyle(
-                      fontFamily: "Cabin-Bold.tff",
                       color: Colors.black,
                       fontWeight: FontWeight.bold,
-                      fontSize: 40,
+                      fontSize: 20,
                       backgroundColor: Colors.white.withOpacity(0.0)),
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(left: width / 24,),
-              child: Text("Unread Messages Counter : "),
-            ),
-             SizedBox(
-              height: height / 64,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: width/12),
-              child: Row( children: [
-                CircleAvatar(backgroundColor: Colors.white,radius: 30,child: IconButton(icon: Icon(Icons.drafts), onPressed: (){}, iconSize: 40,)),
-                SizedBox(width: width/64,),
-                CircleAvatar(backgroundColor: Colors.white,radius: 30,child: IconButton(icon: Icon(Icons.folder), onPressed: (){}, iconSize: 40,)),
-                SizedBox(width: width/64,),
-                CircleAvatar(backgroundColor: Colors.white,radius: 30,child: IconButton(icon: Icon(Icons.folder_delete), onPressed: (){}, iconSize: 40,)),
-                SizedBox(width: width/64,),
-                CircleAvatar(backgroundColor: Colors.white,radius: 30,child: IconButton(icon: Icon(Icons.folder_special), onPressed: (){}, iconSize: 40,)),
-                SizedBox(width: width/64,),
-                CircleAvatar(backgroundColor: Colors.white,radius: 30,child: IconButton(icon: Icon(Icons.person), onPressed: (){}, iconSize: 40,)),
-                //CircleAvatar(backgroundColor: Colors.white,radius: 30,child: TextButton(,onPressed: (){}, child: IconButton(icon: Icon(Icons.person), onPressed: (){}, iconSize: 40,)))
-              ],),
-            ),
             SizedBox(
-              height: height / 32,
+              height: height / 26,
             ),
-            //hena el item builder
             Card(
               child: ListTile(
                 leading: IconButton(
@@ -264,4 +217,3 @@ class _inboxPageState extends State<inboxPage> {
     );
   }
 }
-
