dd5c03eaeef545495acda9f37568142fc08aa002
Abdallah Helmy <abdallahhussam79@gmail.com>
2022-12-14 13:01:06 +0200

    Abdo : Bagarab Media Query

--- lib/inboxPage.dart
+++ lib/inboxPage.dart
@@ -9,38 +9,61 @@ import 'package:badges/badges.dart';
 
 
 class inboxPage extends StatelessWidget {
-  const inboxPage({super.key});
+  var size,height,width;
+  
+
 
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    height = size.height;
+    width = size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        systemOverlayStyle: SystemUiOverlayStyle(
-          statusBarIconBrightness: Brightness.dark,
-          statusBarColor: Colors.grey[350]
-        ),
-        backgroundColor: Colors.grey[350],
-        elevation: 0,
-        toolbarHeight: 300,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 200),
-            child: CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30,)),
-          SizedBox(width: 240),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 200),
-            child: IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,)),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 200),
-            child: Badge(position: BadgePosition.topEnd(top: 35, end: 10),badgeContent: null,child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,))),
+      backgroundColor: Color.fromARGB(255,239,239,239),
+      // appBar: AppBar(
+      //   systemOverlayStyle: SystemUiOverlayStyle(
+      //     statusBarIconBrightness: Brightness.dark,
+      //     statusBarColor: Colors.grey[350]
+      //   ),
+      //   backgroundColor: Colors.grey[350],
+      //   elevation: 0,
+      //   toolbarHeight: 300,
+      //   actions: [
+      //     Padding(
+      //       padding: const EdgeInsets.only(bottom: 200),
+      //       child: CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30,)),
+      //     SizedBox(width: 240),
+      //     Padding(
+      //       padding: const EdgeInsets.only(bottom: 200),
+      //       child: IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,)),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.only(bottom: 200),
+      //       child: Badge(position: BadgePosition.topEnd(top: 35, end: 10),badgeContent: null,child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,))),
+      //     ),
+          
+      //   ],      
+      // ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: height/14),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(width: width/18),
+              CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 32,),
+              SizedBox(width: width/2,),
+              IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 35,)),  
+              Badge(position: BadgePosition.topEnd(top: 10, end: 5),badgeContent: Icon(Icons.circle ,color: Colors.red, size: 4,),child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 35,))),
+            ],
           ),
           
-        ],      
+          
+        ],  
       ),
-      body: Column(children: [
-        Text("Inbox"),
-      ]),
     );
   }
 }
