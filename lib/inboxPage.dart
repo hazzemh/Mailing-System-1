9d5a841ba06341e2a27b126d464e8c402f0c14b0
Abdallah Helmy <abdallahhussam79@gmail.com>
2022-12-14 14:45:36 +0200

    Latest

--- lib/inboxPage.dart
+++ lib/inboxPage.dart
@@ -20,6 +20,7 @@ class inboxPage extends StatelessWidget {
     width = size.width;
 
     return Scaffold(
+      
       backgroundColor: Color.fromARGB(255,239,239,239),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -30,13 +31,14 @@ class inboxPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               SizedBox(width: width/18),
-              CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30,),
+              CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30, backgroundColor: Color.fromARGB(255,239,239,239), child: IconButton(icon: Icon(color: Colors.transparent, Icons.person), onPressed: (){},)),
               SizedBox(width: width/2,),
-              IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,)),  
-              Badge(position: BadgePosition.topEnd(top: 10, end: 10),badgeContent: Icon(Icons.circle ,color: Colors.red, size: 2,),child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,))),
+              IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,), splashRadius: 25),  
+              Badge(position: BadgePosition.topEnd(top: 10, end: 10),badgeContent: Icon(Icons.circle ,color: Color.fromARGB(255,212,64,64), size: 2,),child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,), splashRadius: 25)),
             ],
           ),
-          
+          SizedBox(height: height/10),
+          DropdownButton(items: null, onChanged: null, hint: Text("Inbox", style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold)),),
           
         ],  
       ),
