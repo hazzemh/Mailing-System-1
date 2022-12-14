a5dd1c75ad35863de9e31ec69287341ed07b2706
basselamr <106431358+basselamr@users.noreply.github.com>
2022-12-14 16:04:47 +0200

    Update inboxPage.dart

--- lib/AppViews/inboxPage.dart
+++ lib/AppViews/inboxPage.dart
@@ -1,5 +1,6 @@
 
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, unnecessary_string_interpolations
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -7,11 +8,15 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:badges/badges.dart';
 
+class inboxPage extends StatefulWidget {
+  @override
+  State<inboxPage> createState() => _inboxPageState();
+}
 
-class inboxPage extends StatelessWidget {
-  var size,height,width;
-  
+class _inboxPageState extends State<inboxPage> {
+  var size, height, width;
 
+  var selectedfolder = 'Inbox';
 
   @override
   Widget build(BuildContext context) {
@@ -20,29 +25,222 @@ class inboxPage extends StatelessWidget {
     width = size.width;
 
     return Scaffold(
-      
-      backgroundColor: Color.fromARGB(255,239,239,239),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(height: height/18),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(width: width/18),
-              CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30, backgroundColor: Color.fromARGB(255,239,239,239), child: IconButton(icon: Icon(color: Colors.transparent, Icons.person), onPressed: (){},)),
-              SizedBox(width: width/2,),
-              IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,), splashRadius: 25),  
-              Badge(position: BadgePosition.topEnd(top: 10, end: 10),badgeContent: Icon(Icons.circle ,color: Color.fromARGB(255,212,64,64), size: 2,),child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,), splashRadius: 25)),
-            ],
-          ),
-          SizedBox(height: height/10),
-          DropdownButton(items: null, onChanged: null, hint: Text("Inbox", style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold)),),
-          
-        ],  
+      backgroundColor: Color.fromARGB(255, 239, 239, 239),
+      // appBar: AppBar(
+      //   systemOverlayStyle: SystemUiOverlayStyle(
+      //     statusBarIconBrightness: Brightness.dark,
+      //     statusBarColor: Colors.grey[350]
+      //   ),
+      //   backgroundColor: Colors.grey[350],
+      //   elevation: 0,
+      //   toolbarHeight: 300,
+      //   actions: [
+      //     Padding(
+      //       padding: const EdgeInsets.only(bottom: 200),
+      //       child: CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30,)),
+      //     SizedBox(width: 240),
+      //     Padding(
+      //       padding: const EdgeInsets.only(bottom: 200),
+      //       child: IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,)),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.only(bottom: 200),
+      //       child: Badge(position: BadgePosition.topEnd(top: 35, end: 10),badgeContent: null,child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,))),
+      //     ),
+
+      //   ],
+      // ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: height / 14),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(width: width / 18),
+                // ignore: prefer_const_constructors
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),
+                  radius: 32,
+                ),
+                SizedBox(
+                  width: width / 2,
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                      size: 35,
+                    )),
+                Badge(
+                    position: BadgePosition.topEnd(top: 10, end: 5),
+                    badgeContent: Icon(
+                      Icons.circle,
+                      color: Colors.red,
+                      size: 4,
+                    ),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.calendar_month,
+                          color: Colors.black,
+                          size: 35,
+                        ))),
+              ],
+            ),
+            SizedBox(
+              height: height / 16,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: width / 12),
+              child: DropdownButton(
+                icon: Icon(Icons.keyboard_double_arrow_down_sharp,
+                    color: Colors.black),
+                items: const ["All Inboxes", "Drafts", "Trash", "Sent"]
+                    .map((e) => DropdownMenuItem(
+                          child: Text("$e"),
+                          value: e,
+                        ))
+                    .toList(),
+                onChanged: (val) {
+                  setState(() {
+                    selectedfolder = val.toString();
+                  });
+                },
+                onTap: null,
+                hint: Text(
+                  'All Inboxes',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      backgroundColor: Colors.white.withOpacity(0.0)),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: height / 26,
+            ),
+            Card(
+              child: ListTile(
+                leading: IconButton(
+                  icon: Icon(Icons.person),
+                  onPressed: null,
+                ),
+                title: Text(
+                  'Bassel',
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                subtitle: Text(
+                  'Email Subject'
+                  "\n"
+                  'This is part of the email body click to view full email',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: IconButton(
+                  icon: Icon(Icons.person),
+                  onPressed: null,
+                ),
+                title: Text(
+                  'Hazem',
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                subtitle: Text(
+                  'Email Subject'
+                  "\n"
+                  'This is part of the email body click to view full email',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: IconButton(
+                  icon: Icon(Icons.person),
+                  onPressed: null,
+                ),
+                title: Text(
+                  'Abdo',
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                subtitle: Text(
+                  'Email Subject'
+                  "\n"
+                  'This is part of the email body click to view full email',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: IconButton(
+                  icon: Icon(Icons.person),
+                  onPressed: null,
+                ),
+                title: Text(
+                  'Azzam',
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                subtitle: Text(
+                  'Email Subject'
+                  "\n"
+                  'This is part of the email body click to view full email',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: IconButton(
+                  icon: Icon(Icons.person),
+                  onPressed: null,
+                ),
+                title: Text(
+                  'Biso',
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                subtitle: Text(
+                  'Email Subject'
+                  "\n"
+                  'This is part of the email body click to view full email',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: IconButton(
+                  icon: Icon(Icons.person),
+                  onPressed: null,
+                ),
+                title: Text(
+                  'Hazem',
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                subtitle: Text(
+                  'Email Subject'
+                  "\n"
+                  'This is part of the email body click to view full email',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-     
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.send_rounded),
+          onPressed: null,
+          backgroundColor: Colors.black),
     );
   }
 }
+
