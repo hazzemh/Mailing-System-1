034027d6a695cad21ccc8af971a064c633ee18bd
Abdallah Helmy <abdallahhussam79@gmail.com>
2022-12-14 13:14:32 +0200

    Latest

--- lib/inboxPage.dart
+++ lib/inboxPage.dart
@@ -21,35 +21,11 @@ class inboxPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Color.fromARGB(255,239,239,239),
-      // appBar: AppBar(
-      //   systemOverlayStyle: SystemUiOverlayStyle(
-      //     statusBarIconBrightness: Brightness.dark,
-      //     statusBarColor: Colors.grey[350]
-      //   ),
-      //   backgroundColor: Colors.grey[350],
-      //   elevation: 0,
-      //   toolbarHeight: 300,
-      //   actions: [
-      //     Padding(
-      //       padding: const EdgeInsets.only(bottom: 200),
-      //       child: CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30,)),
-      //     SizedBox(width: 240),
-      //     Padding(
-      //       padding: const EdgeInsets.only(bottom: 200),
-      //       child: IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,)),
-      //     ),
-      //     Padding(
-      //       padding: const EdgeInsets.only(bottom: 200),
-      //       child: Badge(position: BadgePosition.topEnd(top: 35, end: 10),badgeContent: null,child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,))),
-      //     ),
-          
-      //   ],      
-      // ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          SizedBox(height: height/14),
+          SizedBox(height: height/18),
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
