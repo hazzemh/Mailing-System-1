6ffb53a30ff071d593a7abafe073d43db207788f
Abdallah Helmy <76593230+abdallahhelmy99@users.noreply.github.com>
2022-12-15 00:53:55 +0200

    Abdo's Latest Update
    December 15  12:53 AM

--- lib/AppViews/inboxPage.dart
+++ lib/AppViews/inboxPage.dart
@@ -1,3 +1,4 @@
+
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, unnecessary_string_interpolations
 
@@ -6,6 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:badges/badges.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class inboxPage extends StatefulWidget {
   @override
@@ -25,322 +27,125 @@ class _inboxPageState extends State<inboxPage> {
 
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 239, 239, 239),
-      // appBar: AppBar(
-      //   systemOverlayStyle: SystemUiOverlayStyle(
-      //     statusBarIconBrightness: Brightness.dark,
-      //     statusBarColor: Colors.grey[350]
-      //   ),
-      //   backgroundColor: Colors.grey[350],
-      //   elevation: 0,
-      //   toolbarHeight: 300,
-      //   actions: [
-      //     Padding(
-      //       padding: const EdgeInsets.only(bottom: 200),
-      //       child: CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30,)),
-      //     SizedBox(width: 240),
-      //     Padding(
-      //       padding: const EdgeInsets.only(bottom: 200),
-      //       child: IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,)),
-      //     ),
-      //     Padding(
-      //       padding: const EdgeInsets.only(bottom: 200),
-      //       child: Badge(position: BadgePosition.topEnd(top: 35, end: 10),badgeContent: null,child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,))),
-      //     ),
-
-      //   ],
-      // ),
-      // Column(
-      //   crossAxisAlignment: CrossAxisAlignment.start,
-      //   mainAxisAlignment: MainAxisAlignment.start,
-      //   children: [
-      //     SizedBox(height: height/18),
-      //     Row(
-      //       mainAxisAlignment: MainAxisAlignment.start,
-      //       children: [
-      //         SizedBox(width: width/18),
-      //         CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 30, backgroundColor: Color.fromARGB(255,239,239,239), child: IconButton(icon: Icon(color: Colors.transparent, Icons.person), onPressed: (){},)),
-      //         SizedBox(width: width/2,),
-      //         IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,), splashRadius: 25),
-      //         Badge(position: BadgePosition.topEnd(top: 10, end: 10),badgeContent: Icon(Icons.circle ,color: Color.fromARGB(255,212,64,64), size: 2,),child: IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month, color: Colors.black, size: 30,), splashRadius: 25)),
-      //       ],
-      //     ),
-      //     SizedBox(height: height/10),
-      //     DropdownButton(items: null, onChanged: null, hint: Text("Inbox", style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold)),),
-
-      //   ],
-      // ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: height / 14),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(width: width / 18),
-                // ignore: prefer_const_constructors
-                CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        'https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),
-                    radius: 30,
-                    backgroundColor: Color.fromARGB(255, 239, 239, 239),
-                    child: IconButton(
-                      icon: Icon(color: Colors.transparent, Icons.person),
-                      onPressed: () {},
-                    )),
-                SizedBox(
-                  width: width / 2,
-                ),
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.search,
-                      color: Colors.black,
-                      size: 30,
-                    ),
-                    splashRadius: 25),
-                Badge(
-                    position: BadgePosition.topEnd(top: 10, end: 10),
-                    badgeContent: Icon(
-                      Icons.circle,
-                      color: Color.fromARGB(255, 212, 64, 64),
-                      size: 2,
-                    ),
-                    child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.calendar_month,
-                          color: Colors.black,
-                          size: 30,
-                        ),
-                        splashRadius: 25)),
-              ],
-            ),
-            SizedBox(
-              height: height / 64,
-            ),
-            Container(
-              padding: EdgeInsets.only(
-                left: width / 24,
-              ),
-              child: DropdownButton(
-                icon: Icon(
-                  Icons.arrow_drop_down,
-                  color: Colors.black,
-                  size: 30,
-                ),
-                items: const ["Inbox", "Drafts", "Trash", "Sent"]
-                    .map((e) => DropdownMenuItem(
-                          child: Text("$e"),
-                          value: e,
-                        ))
-                    .toList(),
-                onChanged: (val) {
-                  setState(() {
-                    selectedfolder = val.toString();
-                  });
-                },
-                onTap: null,
-                hint: Text(
-                  'Inbox',
-                  style: TextStyle(
-                      fontFamily: "Cabin-Bold.tff",
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 40,
-                      backgroundColor: Colors.white.withOpacity(0.0)),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                left: width / 24,
-              ),
-              child: Text("Unread Messages Counter : "),
-            ),
-            SizedBox(
-              height: height / 64,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: width / 12),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 30,
-                      child: IconButton(
-                        icon: Icon(Icons.drafts),
-                        onPressed: () {},
-                        iconSize: 40,
-                      )),
-                  SizedBox(
-                    width: width / 64,
-                  ),
-                  CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 30,
-                      child: IconButton(
-                        icon: Icon(Icons.folder),
-                        onPressed: () {},
-                        iconSize: 40,
-                      )),
-                  SizedBox(
-                    width: width / 64,
-                  ),
-                  CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 30,
-                      child: IconButton(
-                        icon: Icon(Icons.folder_delete),
-                        onPressed: () {},
-                        iconSize: 40,
-                      )),
-                  SizedBox(
-                    width: width / 64,
-                  ),
-                  CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 30,
-                      child: IconButton(
-                        icon: Icon(Icons.folder_special),
-                        onPressed: () {},
-                        iconSize: 40,
-                      )),
-                  SizedBox(
-                    width: width / 64,
-                  ),
-                  CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 30,
-                      child: IconButton(
-                        icon: Icon(Icons.person),
-                        onPressed: () {},
-                        iconSize: 40,
-                      )),
-                  //CircleAvatar(backgroundColor: Colors.white,radius: 30,child: TextButton(,onPressed: (){}, child: IconButton(icon: Icon(Icons.person), onPressed: (){}, iconSize: 40,)))
-                ],
-              ),
-            ),
-            SizedBox(
-              height: height / 32,
-            ),
-            //hena el item builder
-            Card(
-              child: ListTile(
-                leading: IconButton(
-                  icon: Icon(Icons.person),
-                  onPressed: null,
-                ),
-                title: Text(
-                  'Bassel',
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
-                subtitle: Text(
-                  'Email Subject'
-                  "\n"
-                  'This is part of the email body click to view full email',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                ),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: IconButton(
-                  icon: Icon(Icons.person),
-                  onPressed: null,
-                ),
-                title: Text(
-                  'Hazem',
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
-                subtitle: Text(
-                  'Email Subject'
-                  "\n"
-                  'This is part of the email body click to view full email',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                ),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: IconButton(
-                  icon: Icon(Icons.person),
-                  onPressed: null,
-                ),
-                title: Text(
-                  'Abdo',
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
-                subtitle: Text(
-                  'Email Subject'
-                  "\n"
-                  'This is part of the email body click to view full email',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                ),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: IconButton(
-                  icon: Icon(Icons.person),
-                  onPressed: null,
-                ),
-                title: Text(
-                  'Azzam',
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
-                subtitle: Text(
-                  'Email Subject'
-                  "\n"
-                  'This is part of the email body click to view full email',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                ),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: IconButton(
-                  icon: Icon(Icons.person),
-                  onPressed: null,
-                ),
-                title: Text(
-                  'Biso',
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
-                subtitle: Text(
-                  'Email Subject'
-                  "\n"
-                  'This is part of the email body click to view full email',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                ),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: IconButton(
-                  icon: Icon(Icons.person),
-                  onPressed: null,
-                ),
-                title: Text(
-                  'Hazem',
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
-                subtitle: Text(
-                  'Email Subject'
-                  "\n"
-                  'This is part of the email body click to view full email',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                ),
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: Brightness.dark),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 0,
       ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.send_rounded),
-          onPressed: null,
-          backgroundColor: Colors.black),
+
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: height / 64),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(width: width / 13),
+              CircleAvatar(backgroundImage: NetworkImage('https://lh3.googleusercontent.com/a/AEdFTp7HB1ZjlorTV0wExaxhYEFjVlpn5ODkxRXx6aSHnw=s288-p-rw-no'),radius: 26, backgroundColor: Color.fromARGB(255,239,239,239), child: IconButton(icon: Icon(color: Colors.transparent, Icons.person), onPressed: (){},)),
+              SizedBox(
+                width: width/2,
+              ),
+             IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.black, size: 30,), splashRadius: 25),
+              Badge(position: BadgePosition.topEnd(top: 10, end: 10),badgeContent: Icon(Icons.circle ,color: Color.fromARGB(255,212,64,64), size: 2,),child: IconButton(onPressed: (){}, icon: Icon(Icons.settings, color: Colors.black, size: 30,), splashRadius: 25)),
+            ],
+          ),
+          SizedBox(
+            height: height / 128,
+          ),
+          Container(alignment: Alignment.center,child: Text("Inbox", textAlign: TextAlign.center, style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Badge(position: BadgePosition.topEnd(top: 10, end: 10),),
+              SizedBox(width: 5,),
+              Container(alignment: Alignment.center,child: Text("10 Unread Messages ", textAlign: TextAlign.center, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),)),
+          ],),
+           SizedBox(
+            height: height / 32,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: width/6),
+            child: Row( children: [
+              CircleAvatar(backgroundColor: Colors.white,radius: 30,child: IconButton(icon: Icon(Icons.drafts, color: Colors.black), onPressed: (){}, iconSize: 40,)),
+              SizedBox(width: width/64,),
+              CircleAvatar(backgroundColor: Colors.white,radius: 30,child: IconButton(icon: Icon(Icons.folder, color: Colors.black), onPressed: (){}, iconSize: 40,)),
+              SizedBox(width: width/64,),
+              CircleAvatar(backgroundColor: Colors.white,radius: 30,child: IconButton(icon: Icon(Icons.folder_delete, color: Colors.black), onPressed: (){}, iconSize: 40,)),
+              SizedBox(width: width/64,),
+              CircleAvatar(backgroundColor: Colors.white,radius: 30,child: IconButton(icon: Icon(Icons.folder_special, color: Colors.black), onPressed: (){}, iconSize: 40,)),
+              //SizedBox(width: width/64,),
+              //CircleAvatar(backgroundColor: Colors.white,radius: 30,child: IconButton(icon: Icon(Icons.settings, color: Colors.black), onPressed: (){}, iconSize: 40,)),
+            ],),
+          ),
+          SizedBox(height: height/32,),
+          //hena el item builder
+          Container(
+            height: 1,
+            width: width,
+            decoration: const BoxDecoration(
+              border: Border(
+                left: BorderSide(
+                  color: Colors.white,
+                  width: 1,
+                ),
+                right: BorderSide(
+                  color: Colors.white,
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return Stack(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: width/32, bottom: height/24),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      //borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+          
+                    ),
+                    height: height/6,
+                    width: width,
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: height/32, left: width/64),
+                      child: Badge(position: BadgePosition.topEnd(top: 5, end: 10),badgeContent: Icon(Icons.circle ,color: Colors.transparent, size: 3,), badgeColor: Colors.blue),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: width/10, top: height/40),
+                    child: Text("Abdallah Hussam", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: width/10, top: height/18),
+                    child: Text("Mail Subject", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: width/10, top: height/12),
+                    child: Text("Mail Content Should Be Shown Here, Mail Content Should Be Shown Here", style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),),
+                  ),         
+                  Padding(
+                    padding: EdgeInsets.only(top: height/38, left: width-70),
+                    child: Text("8:12 PM", style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),),
+                  ),  
+                  ],
+                );
+            },),
+          ),
+          
+          
+          ])
+
     );
   }
 }
+
