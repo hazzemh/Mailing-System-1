85e03ef09a6ef32b43054d660e8e351fdcc54fbf
Hazem Gobran <120488458+hazemmx@users.noreply.github.com>
2022-12-15 02:13:19 +0200

    All boxes ready
    keyboard types and obsecure text for passwords with confirmation set waiting for the backend to check the password

--- lib/AppViews/register_screen.dart
+++ lib/AppViews/register_screen.dart
@@ -6,12 +6,14 @@ import 'package:flutter/src/widgets/framework.dart';
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
-  Widget TextFieldBuilder(String labeltxt, TextInputType x, Icon s) {
+  Widget TextFieldBuilder(
+      String labeltxt, TextInputType x, Icon s, bool obsecure) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       decoration: BoxDecoration(
           color: Colors.grey[350], borderRadius: BorderRadius.circular(20)),
       child: TextFormField(
+        obscureText: obsecure,
         decoration: InputDecoration(
             labelText: labeltxt, border: InputBorder.none, suffixIcon: s),
         keyboardType: x,
@@ -21,66 +23,81 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.only(top: 45, bottom: 45),
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Register',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 40),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFieldBuilder(
-                  'First Name', TextInputType.name, Icon(Icons.abc)),
-              SizedBox(
-                height: 20,
-              ),
-              TextFieldBuilder(
-                  'Last Name', TextInputType.name, Icon(Icons.abc)),
-              SizedBox(
-                height: 20,
-              ),
-              TextFieldBuilder('Email Address', TextInputType.emailAddress,
-                  Icon(Icons.alternate_email)),
-              SizedBox(
-                height: 20,
-              ),
-              TextFieldBuilder('Password', TextInputType.visiblePassword,
-                  Icon(Icons.password)),
-              SizedBox(
-                height: 20,
-              ),
-              TextFieldBuilder('Date of birth', TextInputType.datetime,
-                  Icon(Icons.date_range)),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Theme.of(context).accentColor,
-                ),
-                width: double.infinity,
-                child: MaterialButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Submit',
-                    style: TextStyle(color: Colors.white),
+    return SingleChildScrollView(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(30),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 45, bottom: 45),
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldBuilder('First Name', TextInputType.name,
+                    const Icon(Icons.abc), false),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFieldBuilder('Last Name', TextInputType.name,
+                    const Icon(Icons.abc), false),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFieldBuilder('Email Address', TextInputType.emailAddress,
+                    const Icon(Icons.alternate_email), false),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFieldBuilder('Password', TextInputType.visiblePassword,
+                    const Icon(Icons.password), true),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFieldBuilder(
+                    'Confirm Password',
+                    TextInputType.visiblePassword,
+                    const Icon(Icons.password),
+                    true),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFieldBuilder('Phone Number', TextInputType.number,
+                    const Icon(Icons.phone), false),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFieldBuilder('Date of birth', TextInputType.datetime,
+                    const Icon(Icons.date_range), false),
+                const SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Theme.of(context).accentColor,
+                  ),
+                  width: double.infinity,
+                  child: MaterialButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Submit',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
